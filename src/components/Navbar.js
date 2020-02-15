535d1cea4279b7c7ee73219b385ba1a5d1eeb03a
Mark Sarpong <markus20@outlook.com>
2020-02-15 09:52:03 +0000

    Fix brand href issue

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,7 +17,7 @@ const Header = props => {
   return (
     <div style={navContainer}>
       <Navbar expand="lg">
-        <Navbar.Brand href="/#">Punk Beer</Navbar.Brand>
+        <Navbar.Brand href="#">Punk Beer</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="nav navbar-nav ml-auto">
