d9a342e6d19a109d21a5c0a51067b55238046686
Mark Sarpong <markus20@outlook.com>
2020-02-15 05:19:08 +0000

    gh-pages setup

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-     <Router>
+     <Router basename="/">
         <App />
      </Router>
     , document.getElementById('root'));
