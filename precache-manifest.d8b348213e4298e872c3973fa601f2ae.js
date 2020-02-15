48592e069fba3e2bbb939104733fc6868bef984b
Mark Sarpong <markus20@outlook.com>
2020-02-15 04:50:51 +0000

    Updates

---
+++ precache-manifest.d8b348213e4298e872c3973fa601f2ae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c77631d9ef59a29e4390f6238d110f1",
+    "url": "/reacthooks/index.html"
+  },
+  {
+    "revision": "b124260e6a9e2ce8ad2f",
+    "url": "/reacthooks/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "db1bf9495cfd17623631",
+    "url": "/reacthooks/static/css/main.fdf5638f.chunk.css"
+  },
+  {
+    "revision": "b124260e6a9e2ce8ad2f",
+    "url": "/reacthooks/static/js/2.654bcdbc.chunk.js"
+  },
+  {
+    "revision": "f5c04ee9cc323e8805561be7ebbdd5a3",
+    "url": "/reacthooks/static/js/2.654bcdbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db1bf9495cfd17623631",
+    "url": "/reacthooks/static/js/main.3b0d3042.chunk.js"
+  },
+  {
+    "revision": "32db5629f44ba852ebe2",
+    "url": "/reacthooks/static/js/runtime-main.28a651ba.js"
+  },
+  {
+    "revision": "040cfa2ba4fb149df26daccd263b5a07",
+    "url": "/reacthooks/static/media/background1.040cfa2b.jpg"
+  }
+]);
